474763f1c249f811a0c81bf477f971b5ee0f9c8e
奕兵 <215370196@qq.com>
2020-03-16 22:11:39 +0800

    chinese supported

--- DESUtil.py
+++ DESUtil.py
@@ -1,5 +1,5 @@
 def add_pads_if_necessary(s):
-    """adding bits to make an integer number of 64-bit blocks
+    """如果s长度不是64的整数倍，则补0
     """
     number_of_vacancy = len(s) % 64
     need_pads = number_of_vacancy > 0
@@ -14,12 +14,12 @@ def get_bits(plaintext):
         text_bits.extend(to_binary(i))
     return text_bits
 
+# 获取1个Byte的8bit
 def to_binary(s):
-    # if isinstance(s, int):
-    #     n = s
-    # else:
-    #     n = ord(s)
-    n = ord(s)
+    if isinstance(s, int):
+        n = s
+    else:
+        n = ord(s)
     b = [0, 0, 0, 0, 0, 0, 0, 0]
     for i in range(0, 8):
         if n % 2:
@@ -27,6 +27,7 @@ def to_binary(s):
         n = n // 2
     return b
 
+# 左移
 def left_shift(s, times):
     for i in range(times):
         s.append(s.pop(0))
--- DesOperate.py
+++ DesOperate.py
@@ -8,17 +8,20 @@ class DesOperate():
         self.__m_arrOutKey = []
 
     def encry(self, text, keytext):
+        # 产生子密钥
         self.make_keys(key_text=keytext)
-        # print(self.__m_arrOutKey)
+        # 将字符串转为utf-8编码用byte处理
+        text = text.encode()
         text_bits = get_bits(text)
+        # 非64位整数倍，末尾补0
         text_bits = add_pads_if_necessary(text_bits)
 
         result = ''
+        # 每64为一组分隔处理
         for i in range(0, len(text_bits), 64):
             result += self.DES(text_bits, i, (i + 64))
-
         # print(result)
-
+        # 转16进制字符串
         hex_cipher = ''
         i = 0
         while i < len(result):
@@ -28,46 +31,47 @@ class DesOperate():
         return hex_cipher
 
     def decry(self, cipher, key_text):
+        # 产生子密钥
         self.make_keys(key_text)
 
         text_bits = []
         ciphertext = ''
         for i in cipher:
-            # conversion of hex-decimal form to binary form
+            # 16进制转2进制字符串
             tempstr = str(bin(int(i, 16)))[2:]
+            # 开头补0个数
             dislen = 4 - len(tempstr)
             ciphertext += '0'*dislen
             ciphertext += tempstr
-        # print(ciphertext)
         for i in ciphertext:
             text_bits.append(int(i))
-
         text_bits = add_pads_if_necessary(text_bits)
+        # 逆过程
         self.__m_arrOutKey.reverse()
         bin_mess = ''
         for i in range(0, len(text_bits), 64):
             bin_mess += self.DES(text_bits, i, (i + 64))
-
         i = 0
-        text_mess = ''
+        # 以Byte存储结果
+        text_mess = bytearray(b'')
         while i < len(bin_mess):
-            # print(bin_mess[i:i+8])
-            text_mess += chr(int((bin_mess[i:i + 8]), 2))
-            # text_mess += ('\\x%x' % int((bin_mess[i:i + 8]), 2))
+            text_mess.append(int((bin_mess[i:i + 8]), 2))
             i = i + 8
-        return text_mess
+        # 返回字符串
+        return text_mess.decode()
 
     def DES(self, text_bits, start, end):
         block = []
         for i in range(start, end):
             block.append(text_bits[i])
-
+        # ip置换
         block = self.apply_IP(block)
         # print('ip', block)
 
         left_block = block[0:32]
         right_block = block[32:64]
 
+        # 16次迭代
         left_block, right_block = self.iterate(left_block, right_block)
         # print(left_block)
         # print(right_block)
@@ -76,14 +80,15 @@ class DesOperate():
         block.extend(right_block)
         block.extend(left_block)
 
+        # 逆ip置换
         block = self.apply_FP(block)
-        # print(block)
 
         cipher_block = ''
         for i in block:
             cipher_block += str(i)
         return cipher_block
 
+    # ip置换
     def apply_IP(self, block):
         r = []
         r.extend(block)
@@ -91,6 +96,7 @@ class DesOperate():
             r[i] = block[pc_first[i] - 1]
         return r
 
+    # 逆置换
     def apply_FP(self, block):
         """final permutaion
         """
@@ -100,23 +106,19 @@ class DesOperate():
             r[i] = block[pc_last[i] - 1]
         return r
 
+    # e匣子置换
     def e_box(self, block):
-        """exapansion permutation to expand 32 bit block into 48 bit block
-           also called E-box
-        """
         dummy = []
         for i in range(48):
             dummy.append(block[des_E[i] - 1])
-
         r = []
         for i in range(0, 48, 6):
             j = i + 6
             r.append(dummy[i:j])
         return r
 
+    # s匣子置换
     def s_box(self, block):
-        """s-box function to reduce 48 bit block to 32 bit block
-        """
         r = []
         for i in range(0, 8):
             temp = [str(item) for item in block[i]]
@@ -125,6 +127,7 @@ class DesOperate():
             r.extend(binary[4:8])
         return r
 
+    # p匣子置换
     def p_box(self, block):
         """p-box permutation
         """
@@ -134,9 +137,8 @@ class DesOperate():
             r[i] = block[des_P[i] - 1]
         return r
 
+    # 16次迭代
     def iterate(self, left_block, right_block):
-        """iterating  for the 16 rounds
-        """
         for j in range(0, 16):
             d9 = []
             d9.extend(right_block)
@@ -149,12 +151,11 @@ class DesOperate():
             right_block = self.p_box(right_block)
             for i in range(0, 32):
                 right_block[i] ^= left_block[i]
-
             left_block = []
             left_block.extend(d9)
-
         return left_block, right_block
 
+    # 产生子密钥
     def make_keys(self, key_text):
         self.__m_arrOutKey = []
         key = []
@@ -166,9 +167,6 @@ class DesOperate():
             C.append(key[keyleft[i] - 1])
         for i in range(28):
             D.append(key[keyright[i] - 1])
-
-        # print('before make key:', C, D)
-
         for i in range(0, 16):
             if lefttable[i] == 1:
                 C = left_shift(C, 1)
@@ -186,7 +184,7 @@ class DesOperate():
 
 if __name__ == '__main__':
     desop = DesOperate()
-    string = 'Hello world!'
+    string = 'Hello你，好,world去！！2a'
     result = desop.encry(string, 'aaaaaaaa')
     print(result)
     result = desop.decry(result, 'aaaaaaaa')
